92b272696cc89ab2bf4815a73f52e81bb3d780a1
Edward Gregory <grego.dabeatmaker@yahoo.com>
2022-04-19 22:23:45 -0400

    Created Order

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,21 +1,43 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
-//import { createOrder } from '../actions/cartActions'
+import { createOrder } from '../actions/orderActions'
 
 const PlaceOrderScreen = () => {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     const cart = useSelector((state) => state.cart)
 
     const addDecimals = (num) => {
         return (Math.round(num * 100) / 100).toFixed(2)
     }
+    const orderCreate = useSelector((state) => state.orderCreate)
+    const { order, success, error } = orderCreate
+
+    useEffect(() => {
+        if (success) {
+            navigate(`/order/${order._id}`)
+        }
+        // eslint-disable-next-line
+    }, [navigate, success])
+
     const placeOrderHandler = (e) => {
         e.preventDefault()
+        dispatch(
+            createOrder({
+                orderItems: cart.cartItems,
+                shippingAddress: cart.shippingAddress,
+                paymentMethod: cart.paymentMethod,
+                itemsPrice: cart.itemsPrice,
+                shippingPrice: cart.shippingPrice,
+                taxPrice: cart.taxPrice,
+                totalPrice: cart.totalPrice,
+            })
+        )
     }
     // Calculate prices
     cart.itemsPrice = addDecimals(
@@ -49,7 +71,7 @@ const PlaceOrderScreen = () => {
 
                         <ListGroup.Item>
                             <h2>Payment Method</h2>
-                            <strong>Method:</strong>
+                            <strong>Method:</strong> {cart.paymentMethod}
                         </ListGroup.Item>
 
                         <ListGroup.Item>
@@ -124,6 +146,12 @@ const PlaceOrderScreen = () => {
                                     <Col>Total</Col>
                                     <Col>${cart.totalPrice}</Col>
                                 </Row>
+                            </ListGroup.Item>
+
+                            <ListGroup.Item>
+                                {error && (
+                                    <Message variant='danger'>{error}</Message>
+                                )}
                             </ListGroup.Item>
 
                             <ListGroup.Item>
